019dadcd1abf2fa2ca84f6df74b7571b20728d81
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-05 05:41:26 +0100

    Learn about DOM events

--- DOM-manipulation/index.js
+++ DOM-manipulation/index.js
@@ -32,3 +32,13 @@ newP.textContent = 'ME TOO!';
 colorful.appendChild(newP);
 
 container.appendChild(colorful);
+
+// Events
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert('Hello World');
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', e => {
+  console.log(e.target);
+  e.target.style.background = 'blue';
+});
