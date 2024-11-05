8c0fedd1e2bd3a123dddeb2e3b2f6c0371b623cd
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-05 03:16:36 +0100

    Final update

--- Rock-paper-scissors/index.js
+++ Rock-paper-scissors/index.js
@@ -1,38 +1,44 @@
-const getComputerChoice = function() {
+const getComputerChoice = function () {
   const randNr = Math.trunc(Math.random() * 3);
-  
+
   if (randNr === 0) return 'rock';
   if (randNr === 1) return 'paper';
   if (randNr === 2) return 'scissors';
-}
+};
 
-const getHumanChoice = function() {
+const getHumanChoice = function () {
   let choice;
   while (!['rock', 'paper', 'scissors'].includes(choice))
     choice = prompt('Pick ROCK, PAPER or SCISSORS: ').toLowerCase();
-  
+
   if (choice === 'rock') return 'rock';
   else if (choice === 'paper') return 'paper';
   else if (choice === 'scissors') return 'scissors';
   else return alert('Wrong input, try again!');
-}
+};
 
-const playRound = function(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice){
+const playRound = function (humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
     console.log(`Both picked ${humanChoice}. It's a draw.`);
-    return 0; 
-  }
-  else if (humanChoice === 'rock' && computerChoice === 'paper' || humanChoice === 'paper' && computerChoice === 'scissors' || humanChoice === 'scissors' && computerChoice === 'rock') {
-    console.log(`You picked ${humanChoice}, computer picked ${computerChoice}. You lose!`);
+    return 0;
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'paper') ||
+    (humanChoice === 'paper' && computerChoice === 'scissors') ||
+    (humanChoice === 'scissors' && computerChoice === 'rock')
+  ) {
+    console.log(
+      `You picked ${humanChoice}, computer picked ${computerChoice}. You lose!`
+    );
     return 2;
-  }
-  else {
-    console.log(`You picked ${humanChoice}, computer picked ${computerChoice}. You win!`);
+  } else {
+    console.log(
+      `You picked ${humanChoice}, computer picked ${computerChoice}. You win!`
+    );
     return 1;
   }
-}
+};
 
-const playGame = function() {
+const playGame = function () {
   let hScore = 0;
   let cScore = 0;
 
@@ -44,10 +50,14 @@ const playGame = function() {
   }
 
   if (hScore > cScore)
-    console.log(`Total score is ${hScore} : ${cScore} in favor of player. YOU WIN!!`);
+    console.log(
+      `Total score is ${hScore} : ${cScore} in favor of player. YOU WIN!!`
+    );
   else if (hScore < cScore)
-    console.log(`Total score is ${cScore} : ${hScore} in favor of computer. You lose.. :(`);
+    console.log(
+      `Total score is ${cScore} : ${hScore} in favor of computer. You lose.. :(`
+    );
   else console.log("It's a draw.");
-}
+};
 
 playGame();
