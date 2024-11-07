1c5ff3638f7229b09e191a7687bf4ffbbe5f7c04
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-07 02:54:18 +0100

    Hover over the grid

--- Etch-a-sketch/index.js
+++ Etch-a-sketch/index.js
@@ -10,6 +10,13 @@ const createDivs = function () {
       const div2 = document.createElement('div');
       div2.classList.add('innerDiv');
       div.appendChild(div2);
+
+      div2.addEventListener('mouseenter', function (e) {
+        div2.style.backgroundColor = 'black';
+      });
+      div2.addEventListener('mouseleave', function () {
+        div2.style.backgroundColor = 'white';
+      });
     }
   }
 };
