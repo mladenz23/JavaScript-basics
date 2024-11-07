0e369348e0602a9be5ae47d391f84c1ef41caff5
mladenz23 <mladen.zeljkovic@gmail.com>
2024-11-07 05:41:59 +0100

    Add grid size button and functionality

--- Etch-a-sketch/index.js
+++ Etch-a-sketch/index.js
@@ -1,24 +1,57 @@
 const container = document.querySelector('#container');
+const btn = document.querySelector('#btn');
+
+let div;
+let div2;
+const innerDivs = [];
+
+const contStyles = window.getComputedStyle(container);
+const contWidth = contStyles.getPropertyValue('width');
+const contHeight = contStyles.getPropertyValue('height');
+
+let popup = 16;
 
 const createDivs = function () {
-  for (let i = 1; i < 17; i++) {
-    const div = document.createElement('div');
+  for (let i = 0; i < popup; i++) {
+    div = document.createElement('div');
     div.classList.add('outerDiv');
     container.appendChild(div);
 
-    for (let j = 1; j < 17; j++) {
-      const div2 = document.createElement('div');
+    for (let j = 0; j < popup; j++) {
+      div2 = document.createElement('div');
       div2.classList.add('innerDiv');
       div.appendChild(div2);
 
-      div2.addEventListener('mouseenter', function (e) {
-        div2.style.backgroundColor = 'black';
-      });
-      div2.addEventListener('mouseleave', function () {
-        div2.style.backgroundColor = 'white';
-      });
+      innerDivs.push(div2);
     }
   }
 };
 
-createDivs();
+const changeSquareNumber = function () {
+  btn.addEventListener('click', function () {
+    popup = +prompt('Enter the number of squares per line (MAX 100): ');
+    createDivs();
+
+    const squareWidth = (div2.style.width =
+      parseFloat(contWidth) / popup).toString();
+    const squareHeight = (div2.style.height =
+      parseFloat(contHeight) / popup).toString();
+
+    // Fix stacking of grids on multiple button entries
+    innerDivs.forEach(div => {
+      div.setAttribute(
+        'style',
+        `width: ${squareWidth}px; height: ${squareHeight}px;`
+      );
+
+      // div.addEventListener('mouseenter', function () {
+      //   div.style.backgroundColor = 'black';
+      // });
+      // div.addEventListener('mouseleave', function () {
+      //   div.style.backgroundColor = 'white';
+      // });
+    });
+  });
+};
+
+changeSquareNumber();
